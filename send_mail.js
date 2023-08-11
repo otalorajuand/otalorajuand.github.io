dc1a01d83e9d753a2c663cb97db8db23caec3746
Otalorajuand <otalorajuand@gmail.com>
2023-08-11 14:13:42 -0500

    Adjust the deleted of fields

--- send_mail.js
+++ send_mail.js
@@ -8,4 +8,14 @@ function SendMail() {
     emailjs.send("service_uuyafhx","template_bxukjqt", params).then(function (res) {
         alert('Email Sent!');
     })
+    setTimeout(delete_fields, 4000);
+
+    function delete_fields() {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+      }
+    
+
 }
